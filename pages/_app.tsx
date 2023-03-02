a77de921e3d661ae4c47dd088abf18a4251ccc2d
Charles1403 <osehaeguale@gmail.com>
2023-03-02 13:04:49 +0100

    add styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,72 @@
-import type { AppProps } from "next/app";
+import * as React from "react";
+import axios from "utils/axios";
+import { useRouter } from "next/router";
+import customTheme from "../utils/customTheme";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ToastProvider } from "react-toast-notifications";
+import "react-notifications/lib/notifications.css";
+import { NotificationManager } from "react-notifications";
+import { NotificationContainer } from "react-notifications";
+import PageLoader from "components/pageLoader";
+import * as gtag from "utils/gtag";
+import UserService from "utils/UserService";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface Props {
+  Component: React.FC;
+  pageProps: any;
 }
+
+const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
+  const [loaded, setLoaded] = React.useState(false);
+
+  const router = useRouter();
+
+  React.useEffect(() => {
+    axios.interceptors.response.use(
+      (res) => Promise.resolve(res),
+      (err) => {
+        if (!err.response) {
+          NotificationManager.error("Check your network connection");
+        }
+
+        if (err.response.status == 401 && router.pathname !== "/login") {
+          setTimeout(() => {
+            router.replace("/login");
+            typeof window != "undefined" && UserService.clearCredentials();
+          }, 0);
+        }
+        return Promise.reject(err);
+      },
+    );
+    // eslint-disable-next-line
+  }, []);
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 100);
+  }, []);
+  return (
+    <ToastProvider autoDismiss autoDismissTimeout={3000}>
+      <ChakraProvider resetCSS theme={customTheme}>
+        <PageLoader loaded={loaded} />
+        <Box d={loaded ? "block" : "none"}>
+          <Component {...pageProps} />
+          <NotificationContainer />
+        </Box>
+      </ChakraProvider>
+    </ToastProvider>
+  );
+};
+
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../utils/gtag";
+const isProduction = process.env.NODE_ENV === "production";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+                }}
+              />
+              <script
+                data-ad-client="ca-pub-6223283242649033"
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+              />
+              <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
